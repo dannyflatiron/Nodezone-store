c6998085ace45e9155d3b9db13e15c5a12893a79
dannyflatiron <dannyflatiron@gmail.com>
2020-10-21 16:58:21 -0400

    refactor: remove route logic

--- app.js
+++ app.js
@@ -1,43 +1,7 @@
 const http = require('http')
-// file system core module
-const fs = require('fs')
 
-const server = http.createServer((request, response) => {
-  const url = request.url
-  const method = request.method
-  if (url === '/') {
-    response.write('<html>')
-    response.write('<head><title>My First Page</title><head>')
-    response.write('<body><form action="/message" method="POST"><input name="message" type="text"><button type="submit">Send</button></input></form></body>')
-    response.write('</html>')
-    return response.end()
-  }
+const routes = require('./routes')
 
-  // how to redirect
-  if (url === '/message' && method === 'POST') {
-    const body = []
-    // how to parse data
-    request.on('data', (chunk) => {
-      console.log(chunk)
-      body.push(chunk)
-    })
-    return request.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString()
-      const message = parsedBody.split('=')[1]
-      // creates a file called message.txt that has the contents of message
-      fs.writeFile('message.txt', message, (err) => {
-        response.statusCode = 302
-        response.setHeader('Location', '/')
-        return response.end()
-      })
-    })
-  }
-  response.setHeader('Content-Type', 'text/html')
-  response.write('<html>')
-  response.write('<head><title>My First Page</title><head>')
-  response.write('<body><h1>Hello from my Node.js Server!</h1></body>')
-  response.write('</html>')
-  response.end()
-})
+const server = http.createServer(routes.handler)
 
 server.listen(3000)
