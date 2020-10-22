707e71f3d553a575d9a7d26488258be51bce0677
dannyflatiron <dannyflatiron@gmail.com>
2020-10-22 16:52:39 -0400

    feat: view products in console

--- routes/shop.js
+++ routes/shop.js
@@ -5,8 +5,10 @@ const express = require('express')
 const rootDir = require('../utilities/path')
 
 const router = express.Router()
+const adminData = require('./admin')
 
 router.get('/', (request, response, next) => {
+  console.log(adminData.products)
   response.sendFile(path.join(rootDir, 'views', 'shop.html'))
 })
 
