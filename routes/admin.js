8ea22027bf73f0a7baee56a16a3195d9eb26b4bc
dannyflatiron <dannyflatiron@gmail.com>
2020-10-21 23:50:06 -0400

    feat: url has admin in front of it now

--- routes/admin.js
+++ routes/admin.js
@@ -3,10 +3,10 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/add-product', (request, response, next) => {
-  response.send('<h1>The "Add Product" Page</h1><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+  response.send('<h1>The "Add Product" Page</h1><form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
 })
 
-router.post('/product', (request, response, next) => {
+router.post('/add-product', (request, response, next) => {
   console.log(request.body)
   response.redirect('/')
 })
