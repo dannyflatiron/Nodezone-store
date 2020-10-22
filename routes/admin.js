c8f41ef60e6bee4ff88dd21499767b40fe929a51
dannyflatiron <dannyflatiron@gmail.com>
2020-10-22 18:56:22 -0400

    feat: working with html engine handlebars

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,14 @@ const router = express.Router()
 const products = []
 
 router.get('/add-product', (request, response, next) => {
-  response.render('add-product', { pageTitle: "Add Product", path: '/admin/add-product' })
+  response.render('add-product', 
+  { 
+    pageTitle: "Add Product", 
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  })
 })
 
 router.post('/add-product', (request, response, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,13 @@ const router = express.Router()
 
 router.get('/', (request, response, next) => {
   const products = adminData.products
-  response.render('shop', { prods: products, pageTitle: 'Shop', path: '/', hasProducts: products.length > 0 })
+  response.render('shop', { 
+    prods: products, 
+    pageTitle: 'Shop', 
+    path: '/', 
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCss: true})
 })
 
 module.exports = router
