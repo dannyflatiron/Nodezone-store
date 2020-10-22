1d186e470f31dc03f16207e4cdb84b7e8085efbd
dannyflatiron <dannyflatiron@gmail.com>
2020-10-21 23:31:17 -0400

    feat: parse incoming data with body-parser. limit middleware with post

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const http = require('http')
-
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use((request, response, next) => {
-  next()
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/add-product', (request, response, next) => {
+  response.send('<h1>The "Add Product" Page</h1><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
 })
 
-app.use((request, response, next) => {
-  response.send('')
+app.post('/product', (request, response, next) => {
+  console.log(request.body)
+  response.redirect('/')
 })
+
+app.use('/', (request, response, next) => {
+  response.send('<h1>Hello from Express!</h1>')
+})
+
 
 app.listen(3000)
