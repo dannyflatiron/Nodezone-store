31298a77ca908c2103e29049852b00f974b72ccd
dannyflatiron <dannyflatiron@gmail.com>
2020-10-21 23:42:51 -0400

    refactor: refactored routes

--- app.js
+++ app.js
@@ -3,20 +3,15 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use('/add-product', (request, response, next) => {
-  response.send('<h1>The "Add Product" Page</h1><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
-})
+app.use(adminRoutes)
+app.use(shopRoutes)
 
-app.post('/product', (request, response, next) => {
-  console.log(request.body)
-  response.redirect('/')
-})
 
-app.use('/', (request, response, next) => {
-  response.send('<h1>Hello from Express!</h1>')
-})
 
 
 app.listen(3000)
