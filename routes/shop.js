8036e4a312f65d44710a21d4186bbaf3ae374245
dannyflatiron <dannyflatiron@gmail.com>
2020-10-22 18:29:07 -0400

    feat: capture boolean value of hasProducts since handlebars does not perform if statements it only takes single values

--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,7 @@ const router = express.Router()
 
 router.get('/', (request, response, next) => {
   const products = adminData.products
-  response.render('shop', { prods: products, pageTitle: 'Shop', path: '/' })
+  response.render('shop', { prods: products, pageTitle: 'Shop', path: '/', hasProducts: products.length > 0 })
 })
 
 module.exports = router
