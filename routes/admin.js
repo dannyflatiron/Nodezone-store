64900d2979a517b610c3d6a5aaa3374af7368279
dannyflatiron <dannyflatiron@gmail.com>
2020-10-21 23:42:04 -0400

    doc: createed routes folder to store all routes

---
+++ routes/admin.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/add-product', (request, response, next) => {
+  response.send('<h1>The "Add Product" Page</h1><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+})
+
+router.post('/product', (request, response, next) => {
+  console.log(request.body)
+  response.redirect('/')
+})
+
+module.exports = router
+
+// app.use('/add-product', (request, response, next) => {
+//   response.send('<h1>The "Add Product" Page</h1><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+// })
+
+// app.post('/product', (request, response, next) => {
+//   console.log(request.body)
+//   response.redirect('/')
+// })
