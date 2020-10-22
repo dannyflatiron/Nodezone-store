47bdd0c29a3f85ab475c4d7c3905d16aa619519c
dannyflatiron <dannyflatiron@gmail.com>
2020-10-22 00:42:37 -0400

    feat: display 404 page

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 
@@ -11,8 +12,9 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
+
 app.use((request, response, next) => {
-  response.status(404).send('<h1>Page not found</h1>')
+  response.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 
