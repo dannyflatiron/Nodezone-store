de688c46db2b02404eb6d5dc976796ffd56469aa
dannyflatiron <dannyflatiron@gmail.com>
2020-10-22 16:52:13 -0400

    feat: save new products in products array

--- routes/admin.js
+++ routes/admin.js
@@ -6,13 +6,16 @@ const rootDir = require('../utilities/path')
 
 const router = express.Router()
 
+const products = []
+
 router.get('/add-product', (request, response, next) => {
   response.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 })
 
 router.post('/add-product', (request, response, next) => {
-  console.log(request.body)
+  products.push({ title: request.body.title })
   response.redirect('/')
 })
 
-module.exports = router
+exports.routes = router
+exports.products = products
