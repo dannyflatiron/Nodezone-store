0da65d82c05ffcd3ac24f3f90c8456c22abf6b18
dannyflatiron <dannyflatiron@gmail.com>
2020-10-23 12:18:31 -0400

    feat: add attributues to Product

--- models/product.js
+++ models/product.js
@@ -15,8 +15,11 @@ const getProductsFromFile = (cb) => {
 }
 
 module.exports = class Product {
-    constructor(t) {
-        this.title = t
+    constructor(title, imageUrl, description, price) {
+        this.title = title,
+        this.description = description,
+        this.imageUrl = imageUrl,
+        this.price = price
     }
 
     save() {
