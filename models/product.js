5d4e23ce19431ab4e0de0d2cfb40dc746c24024a
dannyflatiron <dannyflatiron@gmail.com>
2020-10-23 10:22:40 -0400

    fix: length on undefined products. Add a callback function to delay asynchronus nature

--- models/product.js
+++ models/product.js
@@ -1,4 +1,18 @@
-const products = []
+const fs = require('fs')
+const path = require('path')
+const p = path.join(
+    path.dirname(require.main.filename), 
+    'data', 
+    'products.json'
+    )
+const getProductsFromFile = (cb) => {
+    fs.readFile(p, (error, fileContent) => {
+        if (error) {
+           return cb([])
+        }
+        cb(JSON.parse(fileContent))
+    })
+}
 
 module.exports = class Product {
     constructor(t) {
@@ -6,10 +20,15 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this)
+        getProductsFromFile(products => {
+            products.push(this)
+            fs.writeFile(p, JSON.stringify(products), (error) => {
+                console.log(error)
+            })
+        })
     }
 
-    static fetchAll() {
-        return products
+    static fetchAll(cb) {
+        getProductsFromFile(cb)
     }
 }
