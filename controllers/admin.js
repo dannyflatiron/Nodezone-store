d137b5dcc37495fc20d94fc9fb1601b68622668d
dannyflatiron <dannyflatiron@gmail.com>
2020-10-25 00:34:07 -0400

    feat&style: changed render route for editing product. check if user is in product editing mode. grab custom query to determine editMode boolean. update product information

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,13 +1,10 @@
 const Product = require('../models/product')
 
 exports.getAddProduct = (request, response, next) => {
-    response.render('admin/add-product', 
-    { 
+    response.render('admin/edit-product', { 
       pageTitle: "Add Product", 
       path: '/admin/add-product',
-      formsCSS: true,
-      productCSS: true,
-      activeAddProduct: true
+      editing: false
     })
   }
 
@@ -17,9 +14,39 @@ exports.getAddProduct = (request, response, next) => {
     const price = request.body.price
     const description = request.body.description
 
-    const product = new Product(title, imageUrl, description, price)
+    const product = new Product(null, title, imageUrl, description, price)
     product.save()
     response.redirect('/')
+  }
+
+  exports.getEditProduct = (request, response, next) => {
+    const editMode = request.query.edit
+    if(!editMode) {
+      return response.redirect('/')
+    }
+    const prodId = request.params.productId
+    Product.findById(prodId, product => {
+      if(!product) {
+        return response.redirect('/')
+      }
+      response.render('admin/edit-product', { 
+        pageTitle: "Edit Product", 
+        path: '/admin/edit-product',
+        editing: editMode,
+        product: product
+      })
+    })
+  }
+
+  exports.postEditProduct = (request, response, next) => {
+    const prodId = request.body.productId
+    const updatedTitle = request.body.title
+    const updatedPrice = request.body.price
+    const updatedDesc = request.body.description
+    const updatedImgUrl = request.body.imageUrl
+    updatedProduct = new Product(prodId, updatedTitle, updatedImgUrl, updatedDesc, updatedPrice)
+    updatedProduct.save()
+    response.redirect('/admin/products')
   }
 
   exports.getProducts = (request, response, next) => {
