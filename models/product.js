78f232bd20e40e938b01e8e96905a6b74a214b3f
dannyflatiron <dannyflatiron@gmail.com>
2020-10-31 01:24:03 -0400

    feat: create Product class. save new product instances to mongodb collection products

--- models/product.js
+++ models/product.js
@@ -1,59 +1,24 @@
-// const Cart = require('./cart');
+const getDb = require('../util/database').getDb
 
-// const db = require('../util/database')
-
-// module.exports = class Product {
-//   constructor(id, title, imageUrl, description, price) {
-//     this.id = id;
-//     this.title = title;
-//     this.imageUrl = imageUrl;
-//     this.description = description;
-//     this.price = price;
-//   }
-
-//   save() {
-//     return db.execute('INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)', 
-//     [this.title, this.price, this.imageUrl, this.description]
-//     )
-//   }
-
-//   static deleteById(id) {
-
-//   }
-
-//   static fetchAll() {
-//     return db.execute('SELECT * FROM products')
-//   }
-
-//   static findById(id) {
-//     return db.execute('SELECT * FROM products WHERE products.id = ?', [id])
-//   }
-// }
-
-const Sequelize = require('sequelize')
-
-const sequelize = require('../util/database')
-
-const Product = sequelize.define('product', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false
+class Product {
+  constructor(title, price, description, imageUrl) {
+    this.title = title
+    this.price = price
+    this.description = description
+    this.imageUrl = imageUrl
   }
-})
+
+  save() {
+    const db = getDb()
+    return db.collection('products')
+    .insertOne(this)
+    .then(result => {
+      console.log(result)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+}
 
 module.exports = Product
