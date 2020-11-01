f114d03ad3ba1c54b6a31bee63e15907a50bd9b1
dannyflatiron <dannyflatiron@gmail.com>
2020-11-01 03:20:07 -0500

    feat: enable shop routes. Find test user

--- app.js
+++ app.js
@@ -8,34 +8,36 @@ const errorController = require('./controllers/error')
 
 const mongoConnect = require('./util/database').mongoConnect
 
+const User = require('./models/user')
+
 const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 const adminRoutes = require('./routes/admin')
-// const shopRoutes = require('./routes/shop')
+const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use((request, response, next) => {
-  // User.findByPk(1)
-  // .then(user => {
-  //   request.user = user
-  //   next()
-  // })
-  // .catch(error => console.log(error))
-  next()
+  User.findById("5f9e4f090a68196662973ad8")
+  .then(user => {
+    request.user = user
+    next()
+  })
+  .catch(error => console.log(error))
 })
 
 app.use('/admin', adminRoutes)
-// app.use(shopRoutes)
+app.use(shopRoutes)
 
 
 app.use(errorController.get404Page)
 
 
 mongoConnect(() => {
+  
   app.listen(3000)
 })
