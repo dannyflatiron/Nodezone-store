3ff9989be72d5c04958ec4bc70757d8f9bb9ab58
dannyflatiron <dannyflatiron@gmail.com>
2020-11-03 01:35:10 -0500

    feat: update quantity of items in cart. if item doesn't exist in cart add it to cart

--- models/user.js
+++ models/user.js
@@ -24,10 +24,23 @@ class User {
   }
 
   addToCart(product) {
-    // const cartProduct = this.cart.items.findIndex(cp => {
-    //   return cp._id === product._id
-    // })
-    const updatedCart = { items: [{ productId: new ObjectId(product._id), quantity: 1 }] }
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+      // both values are originally objects since ids are stored in BSON object format in mongodb
+      // needs to be converted to string values
+      return cp.productId.toString() === product._id.toString()
+    })
+    let newQuantity = 1
+    const updatedCartItems = [...this.cart.items]
+    // if product in cart already exists then update quantity
+    // else if product doesn't exist in cart then add product to cart items array
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1
+      updatedCartItems[cartProductIndex].quantity = newQuantity
+    } else {
+      updatedCartItems.push({ productId: new ObjectId(product._id), quantity: newQuantity })
+    }
+
+    const updatedCart = { items: updatedCartItems }
     const db = getDb()
     return db.collection('users').updateOne(
       { _id: new ObjectId(this._id) },
