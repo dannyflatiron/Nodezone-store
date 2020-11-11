3a7329088de0d4c152ab7160aad403618d832f09
dannyflatiron <dannyflatiron@gmail.com>
2020-11-10 19:41:56 -0500

    feat: create session for user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,21 @@
+const User = require('../models/user')
+
 exports.getLogin = (request, response, next) => {
   response.render('auth/login', {
     path: '/login',
     pageTitle: "Login",
-    isAuthenticated: request.isLoggedIn
+    isAuthenticated: false
   })
 }
 
 exports.postLogin = (request, response, next) => {
-  request.isLoggedIn = true
-  response.redirect('/')
+  // redirects resets the request object therefore isLoggedIn is inherently undefined when it reaches the view
+    User.findById("5fa9e0a49214c4d76e7cf96d")
+    .then(user => {
+      request.session.isLoggedIn = true
+      request.session.user = user
+      response.redirect('/')
+    })
+    .catch(error => console.log(error))
+  // response.setHeader('Set-Cookie', 'loggedIn=true')
 }
