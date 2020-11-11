b1757d725e4685c88b56f54854b7172a0eab9956
dannyflatiron <dannyflatiron@gmail.com>
2020-11-10 19:38:46 -0500

    feat: initialize mongodb store and create sessions collection. Abstract mongodb server address

--- app.js
+++ app.js
@@ -5,13 +5,20 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const session = require('express-session')
+const MongoDBStore = require('connect-mongodb-session')(session)
 
 const errorController = require('./controllers/error')
 
 
 const User = require('./models/user')
 
+const MONGODB_URI = `mongodb+srv://dannyreina:${process.env.PASSWORD}@cluster0.vnxsz.mongodb.net/shop?retryWrites=true&w=majority`
+
 const app = express()
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  colleciton: 'sessions'
+})
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
@@ -22,16 +29,16 @@ const authRoutes = require('./routes/auth')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(session({secret: `${process.env.SESSION}`, resave: false, saveUninitialized: false}))
+app.use(session({ secret: `${process.env.SESSION}`, resave: false, saveUninitialized: false, store: store }))
 
-app.use((request, response, next) => {
-  User.findById("5fa9e0a49214c4d76e7cf96d")
-  .then(user => {
-    request.user = user
-    next()
-  })
-  .catch(error => console.log(error))
-})
+// app.use((request, response, next) => {
+//   User.findById("5fa9e0a49214c4d76e7cf96d")
+//   .then(user => {
+//     request.user = user
+//     next()
+//   })
+//   .catch(error => console.log(error))
+// })
 
 app.use('/admin', adminRoutes) // leading fitler
 app.use(shopRoutes)
@@ -40,7 +47,7 @@ app.use(authRoutes)
 app.use(errorController.get404Page)
 
 
-mongoose.connect(`mongodb+srv://dannyreina:${process.env.PASSWORD}@cluster0.vnxsz.mongodb.net/shop?retryWrites=true&w=majority`)
+mongoose.connect(MONGODB_URI)
 .then(result => {
   User.findOne().then(user => {
     if (!user) {
