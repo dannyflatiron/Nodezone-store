1e3c1f9288cfbc5cd851c8a3e02ee8086f1c3e5c
dannyflatiron <dannyflatiron@gmail.com>
2020-11-10 19:40:00 -0500

    feat: pass boolean for isAuthenticated

--- controllers/error.js
+++ controllers/error.js
@@ -1,5 +1,5 @@
 exports.get404Page = (request, response, next) => {
     response.status(404).render('404', 
     { pageTitle: 'Page Not Found', path: '/404',
-    isAuthenticated: request.isLoggedIn })
+    isAuthenticated: request.session.isLoggedIn })
   }
