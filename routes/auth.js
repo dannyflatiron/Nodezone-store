c83ce5136d2d20843b06b5548d061d2f401bf597
dannyflatiron <dannyflatiron@gmail.com>
2020-11-14 01:26:16 -0500

    feat: display validation errors for post routes on signup and login

--- routes/auth.js
+++ routes/auth.js
@@ -10,25 +10,31 @@ router.get('/login', authController.getLogin)
 
 router.get('/signup', authController.getSignup)
 
-router.post('/login', authController.postLogin)
+router.post('/login', 
+body('email')
+  .isEmail()
+  .withMessage('Please enter a valid email'),
+body('password', 'Incorrect password, please try again or reset your password')
+.isLength({ min: 5 }),
+authController.postLogin)
 
 router.post('/signup', 
 // check for presence of email
-check('email')
-.isEmail()
-.withMessage('Please enter a valid email')
-.custom((value, { req }) => {
+body('email')
+  .isEmail()
+  .withMessage('Please enter a valid email')
+  .custom((value, { req }) => {
   // check if email already exists in database
-  return User.findOne({email: value})
-    .then(userDoc => {
-      if (userDoc) {
-        return Promise.reject('Email already exists, please pick a different one')
-      }
-    })
+return User.findOne({email: value})
+  .then(userDoc => {
+    if (userDoc) {
+      return Promise.reject('Email already exists, please pick a different one')
+    }
+  })
 }),
 // check for password value in the body of the post request
 body('password', 'Please enter a password with only numbers and text and at least 5 characters')
-.isLength({min: 5}),
+  .isLength({min: 5}),
 // check if confirmed password field matches password field
 body('confirmPassword').custom((value, { req }) => {
   if (value !== req.body.password) {
