2874e674b009198d80d026f0684008f7db13d632
dannyflatiron <dannyflatiron@gmail.com>
2020-11-15 02:12:15 -0500

    add get route for 500 page

--- controllers/error.js
+++ controllers/error.js
@@ -3,3 +3,9 @@ exports.get404Page = (request, response, next) => {
     { pageTitle: 'Page Not Found', path: '/404',
     isAuthenticated: request.session.isLoggedIn })
   }
+
+  exports.get500Page = (request, response, next) => {
+    response.status(500).render('500', 
+    { pageTitle: 'Error', path: '/500',
+    isAuthenticated: request.session.isLoggedIn })
+  }
