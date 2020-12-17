613c677cf65c38dc58d27d361dd1788a09e8e53b
dannyflatiron <dannyflatiron@gmail.com>
2020-12-17 03:58:37 -0500

    feat: add pagination logic to getProducts

--- controllers/shop.js
+++ controllers/shop.js
@@ -7,12 +7,26 @@ const ITEMS_PER_PAGE = 1
 
 
 exports.getProducts = (request, response, next) => {
-  Product.find()
+  const page = +request.query.page || 1
+  let totalItems
+
+  Product.find().countDocuments().then(numProducts => {
+    totalItems = numProducts
+    return Product.find()
+    .skip((page - 1) * ITEMS_PER_PAGE)  
+    .limit(ITEMS_PER_PAGE)
+  })
   .then(products => {
     response.render('shop/product-list', { 
       prods: products, 
-      pageTitle: 'All Products', 
-      path: '/products'
+      pageTitle: 'Products', 
+      path: '/products',
+      currentPage: page,
+      hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+      hasPreviousPage: page > 1,
+      nextPage: page + 1,
+      previousPage: page - 1,
+      lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
     })
   })
   .catch(err => {
